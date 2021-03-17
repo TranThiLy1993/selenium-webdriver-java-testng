f14f5e7014f2231e79a7cbbe7c83db5cf77dddc6
Lenovo <Lenovo@192.168.0.7>
2021-03-17 18:30:39 +0700

    Add Topic 02 (Xpath/CSS)

---
+++ webdriver/api/Topic_02_Xpath.java
@@ -0,0 +1,41 @@
+package api;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+
+public class Topic_02_Xpath {
+  WebDriver driver;
+	
+  @BeforeClass
+  public void beforeClass() {
+	  // mo trinh duyet
+	  driver = new FirefoxDriver();
+	  
+	  // mo app
+	  driver.get("https://www.google.com.vn/?hl=vi");
+  }
+  @Test
+  public void TC_02_Login_With_Invalid_Email() {
+	  
+  }
+  
+  @Test
+  public void TC_01_Login_With_Empty_Email() {
+	  
+  }
+  
+  @Test
+  public void TC_03_Login_With_Incorect_Email() {
+	  
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  // dong trinh duyet
+	  driver.quit();
+  }
+
+}
